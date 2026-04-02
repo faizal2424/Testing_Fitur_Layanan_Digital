49b351e1f510818be00a1b3e2e8fd342e08fd1cc
faizal2424 <muh.faizal111@gmail.com>
2026-04-02 11:05:46 +0700

    fix pdf csv pengajuan layanan

--- src/routes/admin/log-status/+page.svelte
+++ src/routes/admin/log-status/+page.svelte
@@ -102,6 +102,7 @@
 					href="/admin/log-status/export/csv?{$page.url.searchParams.toString()}"
 					class="btn export-btn csv"
 					download
+					data-sveltekit-reload
 				>
 					<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="8" y1="13" x2="16" y2="13"></line><line x1="8" y1="17" x2="16" y2="17"></line></svg>
 					Export CSV
@@ -110,6 +111,7 @@
 					href="/admin/log-status/export/pdf?{$page.url.searchParams.toString()}"
 					class="btn export-btn pdf"
 					target="_blank"
+					data-sveltekit-reload
 				>
 					<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><path d="M16 13H8"></path><path d="M16 17H8"></path></svg>
 					Cetak PDF
